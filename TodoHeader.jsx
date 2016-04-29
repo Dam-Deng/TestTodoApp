ae3b78909dde02ef16c5e66e715d6308afb9f6cb
dam <240242398@qq.com>
2016-04-29 19:24:03 +0800

    add react js

--- TodoHeader.jsx
+++ TodoHeader.jsx
@@ -1,4 +1,16 @@
 const TodoHeader = React.createClass({
+  propTypes: {
+    title: React.PropTypes.string,
+    name: React.PropTypes.string,
+    todoCount: React.PropTypes.number
+  },
+  getDefaultProps() {
+    return {
+      title: "標題",
+      name: "姓名",
+      todoCount: 9999
+    }
+  },
   render () {
     const {title, name, todoCount} = this.props;
     return (
@@ -9,5 +21,24 @@ const TodoHeader = React.createClass({
     )
   }
 })
+
+// const TodoHeader = ({title, name, todoCount}) =>
+//       <div>
+//       <h1>{title}</h1>
+//       <h4>hello {name} : 你還有 {todoCount} 項還有未完成待辦事項</h4>
+//     </div>
+
+// TodoHeader.propTypes = {
+//   title: React.PropTypes.string,
+//   name: React.PropTypes.string,
+//   todoCount: React.PropTypes.number
+// };
+
+// TodoHeader.defaultProps = {
+//   title: "標題",
+//   name: "姓名",
+//   todoCount: 999
+// }
+
 
 window.App.TodoHeader = TodoHeader;
--- js.jsx
+++ js.jsx
@@ -8,9 +8,14 @@ const {
 var TodoApp = React.createClass({
 
   render: function() {
+    const data = {
+      title: "你的待辦清單",
+      name: "dam",
+      todoCount: 8
+    }
     return (
       <div>
-        <TodoHeader title="你的待辦清單" name="dam" todoCount="9"/>
+        <TodoHeader {...data}/>
         <InputField placeholder="新的待辦事項"/>
         <TodoList />
       </div>
